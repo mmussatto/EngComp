a2ee947fefb5ead23a290d6100967b2b2e89c08c
mmussatto <murilomussatto@gmail.com>
2020-04-30 16:23:21 -0300

    Added a comment to Selection Sort

--- ED2/Algoritmos/Ordena.h
+++ ED2/Algoritmos/Ordena.h
@@ -3,7 +3,8 @@
 
 int Bubble_Sort(int v[], int T);
 /*O BubbleSort basicamente vai empurrando o elemento de maior valor para o
-final do vetor, sendo que na proxima iteracao, ele nao precisa olhar a ultima posicao
+final do vetor, fazendo trocas sucessivas entre dois elementos próximos,
+sendo que na proxima iteracao, ele nao precisa olhar a ultima posicao
 ja que o maior elemento do vetor ja esta nela*/
 
 int Quick_Sort(int v[], int b, int n);
@@ -12,3 +13,6 @@ a posicao final desse elemento no vetor ordenado. Entao, o quick sort sera aplic
 dois subvetores criados, a direita e a esquerda do pivot*/
 
 int Selection_Sort(int v[], int T);
+/*O Selection Sort escaneia o vetor procurando o maior elemento e apos acha-lo, o coloca
+na ultima posicao do vetor. O algoritimo roda novamente sem levar em conta a ultima posicao
+pois o maior elemento já está la*/
