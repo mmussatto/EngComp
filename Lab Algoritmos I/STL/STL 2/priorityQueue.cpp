c296412828e26d25472c43126c7f5458ed3d026c
mmussatto <murilomussatto@gmail.com>
2022-03-29 21:35:17 -0300

    Refactoring and adding comments

--- Lab Algoritmos I/STL/STL 2/priorityQueue.cpp
+++ Lab Algoritmos I/STL/STL 2/priorityQueue.cpp
@@ -2,20 +2,15 @@
 #include <queue>
 #include <stack>
 
-
+/*Structed use to remember with job was important*/
 struct job
 {
     int priority;
     bool myjob = false;
-    bool operator<(const job& other)const
-    {
-        if(priority == other.priority)
-            return true;
-
-        return priority < other.priority;
-    }
 };
 
+
+/*Function using for printing the queue. It was using during debugging, not necessary to the solution*/
 void printQueue (std::priority_queue<job> pq)
 {
     while (!pq.empty())
@@ -27,34 +22,32 @@ void printQueue (std::priority_queue<job> pq)
 }
 
 
-
-
 int main ()
 {
 
-    int nTestes;
+    int nTestCases;
 
-    std::cin >> nTestes;
+    std::cin >> nTestCases;
 
-    while (nTestes--)
+    while (nTestCases--)    //run through all the test cases
     {
-
         int n, m;
 
-        std::queue<job> jobQueue;
-        std::priority_queue<int> pq;
+        std::queue<job> jobQueue;   //queue for simulating the procedure
+
+        std::priority_queue<int> pq;    //queue for checking the highest active priority
 
 
-        std::cin >> n >> m;
+        std::cin >> n >> m;     //read number of elements and with one is important
 
         int i = 0;
-        while (i < n)
+        while (i < n)   //iterate through all the elements
         {
             job temp;
 
             std::cin >> temp.priority;
 
-            if(i == m)
+            if(i == m)  //verify if the job is important
             {
                 temp.myjob = true;
             }
@@ -65,32 +58,32 @@ int main ()
             i++;
         }
 
-        i = 0;
-        while (!jobQueue.front().myjob || jobQueue.front().priority != pq.top())
+        i = 0; //reset i
+
+        /*This loop will run until the first job of the jobQueue is the important one and its priority is 
+        the highest active priority*/
+        while (!jobQueue.front().myjob || jobQueue.front().priority != pq.top()) 
         {
             job j = jobQueue.front();
 
-            if(j.priority == pq.top())
+            if(j.priority == pq.top())  //active job has the highest priority -> pop from both queues
             {
                 jobQueue.pop();
                 pq.pop();
                 i++;
             }
-            else
+            else    //active job does not have the highest priority -> put it in the end of the queue
             {
                 jobQueue.pop();
                 jobQueue.push(j);
             }
         }
         
-        i++;
-        std::cout << i << "\n";
+        i++;    //accounts for the pop of the important job
 
-        //printQueue(pq);
+        std::cout << i << "\n"; //print the number of jobs completed 
         
-
     }
     
-
     return 0;
 }
