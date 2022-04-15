a296fc70713bd32f2831a867f9a38b78ee95023e
mmussatto <murilomussatto@gmail.com>
2022-04-15 19:22:54 -0300

    Working solution for GUL 1

---
+++ Lab Algoritmos I/Algoritmos Gulosos/GUL 1/dragonHeads.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<queue>
+
+void clear_pq (std::priority_queue<int, std::vector<int>, std::greater<int>> *pq)
+{
+    while (!pq->empty())
+    {
+        pq->pop();
+    }
+}
+
+int main ()
+{
+    int n_heads, n_knights;
+    int cost = 0;
+
+    std::priority_queue<int, std::vector<int>, std::greater<int>> heads;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> knights;
+
+    std::cin >> n_heads >> n_knights;
+
+    while (n_heads != 0 && n_knights != 0)
+    {
+        //read heads
+        while (n_heads--)
+        {
+            int temp;
+            std::cin >> temp;
+            heads.push(temp);
+        }
+
+        //read knights
+        while (n_knights--)
+        {
+            int temp;
+            std::cin >> temp;
+            knights.push(temp);
+        }
+        
+        while (true)
+        {
+            int top_head = heads.top();
+            int top_knight = knights.top();
+
+            if(top_knight >= top_head)
+            {
+              cost += top_knight; //update cost
+              heads.pop();
+              knights.pop();
+            }
+            else    //knight is smaller than head
+            {
+                knights.pop();
+            }
+
+            if(heads.empty()) //all heads were chopped off
+            {
+                std::cout << cost << "\n";
+                break;
+            }
+            else if(knights.empty())    //no more knights but there are heads remaining
+            {
+                std::cout << "Loowater is doomed!\n";
+                break;
+            }
+        }
+
+        clear_pq(&heads);
+        clear_pq(&knights);
+
+        std::cin >> n_heads >> n_knights;
+        cost = 0;
+    }
+    return 0;
+}
