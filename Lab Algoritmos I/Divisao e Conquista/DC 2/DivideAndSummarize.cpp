912e252c7c2d73f879df44a23e0e95d59b0960b7
mmussatto <murilomussatto@gmail.com>
2022-05-14 22:30:50 -0300

    Working solution for Div&Con 2

--- Lab Algoritmos I/Divisao e Conquista/DC 2/DivideAndSummarize.cpp
+++ Lab Algoritmos I/Divisao e Conquista/DC 2/DivideAndSummarize.cpp
@@ -1,9 +1,55 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <unordered_set>
 
 
-bool test(std::vector<int> a, int s)
+int arraySum(std::vector<int> a)
 {
-    return true;
+    int sum = 0;
+
+    for(auto i : a)
+        sum += i;
+
+    return sum;
+}
+
+void possibleSums (std::vector<int> a, std::unordered_set<int> &set)
+{
+    if(a.size() == 1)
+    {
+        set.insert(a[0]);
+        return;    
+    }
+
+    set.insert(arraySum(a));
+
+    int max = a.back();
+    int min = a.front();
+
+    int mid = (max + min)/2;
+
+
+    std::vector<int> left;
+    std::vector<int> right;
+
+    size_t i;
+    for (i = 0; i < a.size() && a[i] <= mid; i++)
+    {
+        left.push_back(a[i]);
+    }
+    for (; i < a.size(); i++)
+    {
+        right.push_back(a[i]);
+    }
+
+    if(right.empty() || left.empty())
+        return;
+    
+
+    possibleSums(left, set);
+    possibleSums(right, set);
+
 }
 
 int main ()
@@ -19,6 +65,7 @@ int main ()
         std::cin >> n >> q;
 
         std::vector<int> a;
+        std::unordered_set<int> set;
 
         //read array
         int i = n;
@@ -31,31 +78,18 @@ int main ()
 
         std::sort(a.begin(), a.end());
 
+        possibleSums(a, set);
+
         while (q--)
         {
             int s;
             std::cin >> s;
 
-            std::cout << test(a, s) ? "Yes" : "No";
+            set.count(s) != 0 ? std::cout <<  "Yes" : std::cout <<  "No";
+            std::cout << "\n";
+          
         }
-        
-            
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
     return 0;
 }
