f6269a6ef82e8bc3abfcf819df7d552d05b840fb
mmussatto <murilomussatto@gmail.com>
2022-05-14 22:36:37 -0300

    Add coments to Div&Con 2 solution

--- Lab Algoritmos I/Divisao e Conquista/DC 2/DivideAndSummarize.cpp
+++ Lab Algoritmos I/Divisao e Conquista/DC 2/DivideAndSummarize.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <unordered_set>
 
-
+/*Sum of all the elements inside an array*/
 int arraySum(std::vector<int> a)
 {
     int sum = 0;
@@ -14,25 +14,29 @@ int arraySum(std::vector<int> a)
     return sum;
 }
 
+/*Calculate all possible sums of an array. This is a recursive function 
+that gets all the sums of the subarrays and store them inside a unordered_set*/
 void possibleSums (std::vector<int> a, std::unordered_set<int> &set)
 {
-    if(a.size() == 1)
+    if(a.size() == 1)   //array only has one element
     {
         set.insert(a[0]);
         return;    
     }
 
+    //insert the sum of the curent array to the set
     set.insert(arraySum(a));
+
 
     int max = a.back();
     int min = a.front();
-
     int mid = (max + min)/2;
 
-
+    //subarrays
     std::vector<int> left;
     std::vector<int> right;
 
+    //divide array into subarrays left and right
     size_t i;
     for (i = 0; i < a.size() && a[i] <= mid; i++)
     {
@@ -43,10 +47,11 @@ void possibleSums (std::vector<int> a, std::unordered_set<int> &set)
         right.push_back(a[i]);
     }
 
+    //if one of the subarrays is empty, terminate recursion (all elements of the array are the same)
     if(right.empty() || left.empty())
         return;
     
-
+    //recursive calls
     possibleSums(left, set);
     possibleSums(right, set);
 
@@ -76,20 +81,24 @@ int main ()
             a.push_back(temp);
         }
 
+        //sort array
         std::sort(a.begin(), a.end());
 
+        //calculate all possible sums of subarrays
         possibleSums(a, set);
 
+        //prettiness tests 
         while (q--)
         {
+            //read sum
             int s;
             std::cin >> s;
 
+            //if sum is inside of set, print "Yes", if not, print "No"
             set.count(s) != 0 ? std::cout <<  "Yes" : std::cout <<  "No";
             std::cout << "\n";
           
         }
     }
-
     return 0;
 }
