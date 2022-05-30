40a698aafafaf362fc53ab823cc07339caa5f018
mmussatto <murilomussatto@gmail.com>
2022-05-29 23:40:15 -0300

    Addin comments to Knapsack

--- Lab Algoritmos I/Programacao Dinamica/Dp 4/Knapsack.cpp
+++ Lab Algoritmos I/Programacao Dinamica/Dp 4/Knapsack.cpp
@@ -8,40 +8,44 @@ int main ()
     int n, W;
     std::cin >> n >> W;
 
+    //wi, vi
     std::vector<std::vector<long long>> items(n+1, std::vector<long long>(2));
 
-    //read matrix
+    //Read items weight and value
     for (int i = 1; i <= n; i++)
     {
         std::cin >> items[i][0] >> items[i][1];
     }
     
-
+    //[item][max_weight]
     std::vector<std::vector<long long>> dp(n+1, std::vector<long long>(W+1));
 
+    //Iterate through all the items
     for (int i = 0; i <= n; i++)
     {
-        long long wi = items[i][0];
-        long long vi = items[i][1];
+        //current item
+        long long wi = items[i][0]; //weight
+        long long vi = items[i][1]; //value
 
+        //iterate through all the max_weights from 0 to W
         for (int j = 0; j <= W; j++)
         {
-            if(i == 0 || j == 0)
+            if(i == 0 || j == 0)    //no weight or no item selected
                 dp[i][j] = 0;
-            else if(j >= wi)
+            else if(j >= wi)    //verify valid item
             {
-                long long int last = dp[i-1][j];
-                long long int now = vi + dp[i-1][j-wi];
+                long long int last = dp[i-1][j];        //current solution until now
+                long long int now = vi + dp[i-1][j-wi]; //new item plus last best solution
                 dp[i][j] = std::max(last, now);
             }
-            else
+            else    //invalid item
                 dp[i][j] = dp[i-1][j];
         }
         
     }
 
+    //bottom-left corner of the matrix is the best value
     std::cout << dp[n][W];
     
-
     return 0;
 }
